1683c42df17f3b8c8bf3170081527429f0e51a26
Favot <70208301+Favot@users.noreply.github.com>
2023-09-19 22:55:41 +0200

    Create an app file and test for it

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express'
+import { userRouter } from './routes/user'
+
+const app = express()
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Hello, TypeScript with Express!')
+})
+app.use('/user', userRouter)
+
+export { app }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
-import express, { Request, Response } from 'express'
-import { userRouter } from './routes/user'
+import { app } from './app'
 
-const app = express()
 const PORT = 3000
-console.log(process.env.DATABASE_URL)
-app.get('/', (_req: Request, res: Response) => {
-  res.send('Hello, TypeScript with Express!')
-})
-
-app.use('/user', userRouter)
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
