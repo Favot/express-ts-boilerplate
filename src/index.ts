f287fb311a5663e46b3564266dc00e52f5b27f72
Favot <70208301+Favot@users.noreply.github.com>
2023-09-19 11:12:57 +0200

    Update index file with the new route

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-import { PrismaClient } from "@prisma/client";
 import express, { Request, Response } from "express";
+import { userRouter } from "./routes/user";
 
-const prisma = new PrismaClient();
 const app = express();
 const PORT = 3000;
-
+console.log(process.env.DATABASE_URL);
 app.get("/", (_req: Request, res: Response) => {
   res.send("Hello, TypeScript with Express!");
 });
 
-app.get("/users", async (req: Request, res: Response) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
-});
+app.use("/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
