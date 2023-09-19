12d5371a94a1d8d1e11b78ccd5bb2cf472da9548
Favot <70208301+Favot@users.noreply.github.com>
2023-09-19 07:58:26 +0200

    Update server with prisma

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-const init = () => {
-  console.log("Hello World");
-};
+import { PrismaClient } from "@prisma/client";
+import express, { Request, Response } from "express";
 
-init();
+const prisma = new PrismaClient();
+const app = express();
+const PORT = 3000;
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Hello, TypeScript with Express!");
+});
+
+app.get("/users", async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
