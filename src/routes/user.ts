18c9df5e25e9245e65a95dca1112628b40a9254e
Favot <70208301+Favot@users.noreply.github.com>
2023-09-19 11:11:53 +0200

    Create route for User

---
+++ src/routes/user.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  createUser,
+  deleteUser,
+  getUserById,
+  getUsers,
+  updateUser,
+} from "../controllers/user";
+
+const userRouter = express.Router();
+
+userRouter.get("/", getUsers);
+userRouter.get("/:id", getUserById);
+userRouter.post("/", createUser);
+userRouter.put("/:id", updateUser);
+userRouter.delete("/:id", deleteUser);
+
+export { userRouter };
