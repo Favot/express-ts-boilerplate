680bdcf89a0e69eab32660f34e446e28c6b52273
Favot <70208301+Favot@users.noreply.github.com>
2023-09-24 11:33:24 +0200

    Update setup script in a single script

--- scripts/setup/createEnvFile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Construct the DATABASE_URL and write to .env file
-echo "DATABASE_URL=\"postgresql://${postgresUser}:${postgresPassword}@node_express_db:5432/${postgresDb}?schema=public\"" > .env
-
-# Inform the user that the .env file has been created
-echo ".env file has been created with the provided database credentials."
